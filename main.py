3d5f1f4a4b42965870ca548a44d7bedc68fba66e
shelf08 <vip.lobtsov@gmail.com>
2024-02-25 21:41:59 +0300

    need to add tests

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def get_circle_points(x, y, r):
     return [(x + r * i, y) for i in range(-r, r+1)] + [(x, y + r * i) for i in range(-r, r+1)]
 
-# Функция для проверки пересечения двух окружностей
 def do_circles_overlap(circle1, circle2):
     x1, y1, r1 = circle1
     x2, y2, r2 = circle2
@@ -11,14 +10,14 @@ def do_circles_overlap(circle1, circle2):
 circles = []
 output_circles = []
 
-# Чтение координат из файла
+
 with open("input01.txt", "r") as file:
     lines = file.readlines()
     for line in lines:
         x, y, r = map(int, line.strip().split())
         circles.append((x, y, r))
 
-# Находим окружности, которые не пересекаются с другими окружностями
+
 for circle in circles:
     is_overlapping = False
     for other_circle in circles:
@@ -28,6 +27,6 @@ for circle in circles:
     if not is_overlapping:
         output_circles.append(circle)
 
-# Выводим результат
+
 for circle in output_circles:
     print(f"Окружности с координтами центра ({circle[0]}, {circle[1]}) и радиусом {circle[2]} не пересекаются с другими окружностями.")
